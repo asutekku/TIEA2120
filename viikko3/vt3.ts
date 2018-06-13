e6955b3661394c5a13a3684c10412d1c3355f572
asutekku <asutekku@protonmail.com>
2018-06-14 01:08:44 +0300

    Initial commit for viikko3

---
+++ viikko3/vt3.ts
@@ -0,0 +1,14 @@
+// data-muuttuja sisältää kaiken tarvittavan ja on rakenteeltaan lähes samankaltainen kuin viikkotehtävässä 2
+// Rastileimaukset on siirretty tupa-rakenteesta suoraan jokaisen joukkueen yhteyteen
+//
+// voit tutkia tarkemmin käsiteltävää tietorakennetta konsolin kautta 
+// tai json-editorin kautta osoitteessa http://jsoneditoronline.org/
+// Jos käytät json-editoria niin avaa data osoitteesta:
+// http://appro.mit.jyu.fi/tiea2120/vt/vt3/data.json
+
+"use strict";
+
+console.log(data);
+
+
+
